992199d56adca3b72dc347d057dff8556080604b
Ghost1499 <zgstv@yandex.ru>
2024-01-26 23:56:07 +0300

    add image target size parameter

--- src/configs/ds_prepare_config.py
+++ src/configs/ds_prepare_config.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from src.configs import model_config
+
 
 TEST_SIZE = 0.2
 VALIDATION_TRAIN_SIZE = 0.2
@@ -7,3 +9,4 @@ RANDOM_STATE = 0
 RNG = np.random.RandomState(RANDOM_STATE)
 BATCH_SIZE = 32
 DS_SHUFFLE_BUFF_SIZE = BATCH_SIZE * 8
+TARGET_SIZE = model_config.TARGET_SHAPE[0:2]
