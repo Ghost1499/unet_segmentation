d208e0bb38ebe79ba30a5fbcfc1635ae36451c54
Ghost1499 <zgstv@yandex.ru>
2024-01-27 01:45:30 +0300

    add creating contours mask

--- src/scripts/prepare_dataset.py
+++ src/scripts/prepare_dataset.py
@@ -3,11 +3,28 @@ from pathlib import Path
 from typing import Self
 
 from matplotlib.image import imread, imsave
+from matplotlib.pyplot import imshow, show
+import numpy as np
+import numpy.typing as npt
+
 from skimage.transform import resize
+from skimage.morphology import binary_dilation, binary_erosion
+from skimage.morphology.footprints import disk
 from tqdm import tqdm
 
 from src.ImagesDir import ImagesDir
 from src.configs import ds_prepare_config, io_config
+
+KERNEL_RADIUS = 3
+OP_KERNEL = disk(KERNEL_RADIUS, dtype=np.bool_)
+
+
+def create_contours_mask(mask: npt.NDArray[np.uint8]):
+    mask = mask.astype(np.bool_)
+    dilated = binary_dilation(mask, OP_KERNEL)
+    eroded = binary_erosion(mask, OP_KERNEL)
+    contours_mask = dilated & ~eroded
+    return contours_mask.astype(np.uint8) * 255
 
 
 def resizing_process(img_load_path, img_save_path):
@@ -36,6 +53,20 @@ def test():
     resizing_process(img_load_path, img_save_path)
 
 
+def test_cont_mask():
+    mask = imread(
+        Path(
+            r"C:\CSF\programs\Segmentation_cont\data\datasets\carvana_mini\test_masks\0cdf5b5d0ce1_03_mask.gif"
+        )
+    )
+    mask = mask[..., 0]
+    mask = np.where(mask >= 256 / 2, 255, 0)
+    cont_mask = create_contours_mask(mask)
+    imshow(cont_mask)
+    show()
+    imsave("results/0cdf5b5d0ce1_03_mask_mini.png", cont_mask)
+
+
 def main():
     load_folder = io_config.CARVANA_DIR
     save_folder = load_folder.with_name("_".join([load_folder.name, "mini"]))
@@ -43,5 +74,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    test_cont_mask()
     # test()
