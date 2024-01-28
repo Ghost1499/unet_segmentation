2a5ebf537c9d259daa11e20059e6255268726685
Ghost1499 <zgstv@yandex.ru>
2024-01-28 22:08:36 +0300

    fix load functions

--- src/load_samples.py
+++ src/load_samples.py
@@ -3,10 +3,10 @@ from typing import Any
 import numpy as np
 import tensorflow as tf
 
-from configs import model_config
+from configs import ds_prepare_config
 
 
-def load_image(path: str) -> Any:
+def load_resize_image(path: str) -> Any:
     """
     Загружает цветное изображение .jpeg, расположенное в path. Уменьшает до размера TARGET_SIZE и нормирует к диапазону [0,1] float32.
 
@@ -16,14 +16,18 @@ def load_image(path: str) -> Any:
     Returns:
         Any: изображение в виде тензора типа float32
     """
-    image = tf.io.read_file(path)
-    image = tf.io.decode_jpeg(image, channels=3)  # type: ignore
-    image = tf.image.resize(image, model_config.TARGET_SHAPE[0:2])
-    image = image / 255.0  # type: ignore
+    image = load_image(path)
+    image = tf.image.resize(image, ds_prepare_config.TARGET_SIZE)
     return image
 
 
-def load_mask(path: str) -> Any:
+def load_image(path: str) -> Any:
+    image = tf.io.read_file(path)
+    image = tf.io.decode_image(image, channels=3, dtype=tf.dtypes.float32)  # type: ignore
+    return image
+
+
+def load_resize_mask(path: str) -> Any:
     """
     Загружает цветное изображение, расположенное в path. Преобразует в серое, уменьшает до размера TARGET_SIZE и нормирует к диапазону [0,1] типа int8(?).
 
@@ -33,24 +37,40 @@ def load_mask(path: str) -> Any:
     Returns:
         Any: изображение в виде тензора типа int8(?)
     """
+    mask = load_mask(path)
+    mask = tf.image.resize(mask, ds_prepare_config.TARGET_SIZE, method="nearest")
+    return mask
+
+
+def load_mask(path: str) -> Any:
     mask = tf.io.read_file(path)
-    mask = tf.io.decode_image(mask, channels=3, expand_animations=False)
-    mask = tf.image.rgb_to_grayscale(mask)
-    mask = tf.image.resize(mask, model_config.TARGET_SHAPE[0:2], method="nearest")
-    mask = tf.image.convert_image_dtype(mask, dtype=tf.dtypes.uint8)
-    mask = mask / 255  # type: ignore
+    mask = tf.io.decode_image(
+        mask, channels=3, expand_animations=False, dtype=tf.dtypes.float32
+    )
+    mask = tf.expand_dims(tf.unstack(mask, axis=-1)[0], axis=-1)
     return mask
 
 
 def __test():
-    image = load_image(
+    image = load_resize_image(
         r"C:\CSF\programs\Segmentation_cont\data\datasets\carvana\train\0cdf5b5d0ce1_01.jpg"
     ).numpy()
     print(
         f"Image:{image.shape}, {image.dtype}. Values: {image.min()}-{image.max()} [{image.mean()}]"
     )
-    mask = load_mask(
+    mask = load_resize_mask(
         r"C:\CSF\programs\Segmentation_cont\data\datasets\carvana\train_masks\0cdf5b5d0ce1_01_mask.gif"
+    ).numpy()
+    print(f"Mask:{mask.shape}, {mask.dtype}. Values: {np.unique(mask)} [{mask.mean()}]")
+
+    image = load_resize_image(
+        r"C:\CSF\programs\Segmentation_cont\data\datasets\carvana_mini\train\0cdf5b5d0ce1_01.jpg"
+    ).numpy()
+    print(
+        f"Image:{image.shape}, {image.dtype}. Values: {image.min()}-{image.max()} [{image.mean()}]"
+    )
+    mask = load_resize_mask(
+        r"C:\CSF\programs\Segmentation_cont\data\datasets\carvana_mini\train_masks\0cdf5b5d0ce1_01_mask.gif"
     ).numpy()
     print(f"Mask:{mask.shape}, {mask.dtype}. Values: {np.unique(mask)} [{mask.mean()}]")
 
