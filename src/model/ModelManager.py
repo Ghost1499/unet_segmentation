88a081f813fae2da1f76e7b85d677869830e947a
Ghost1499 <zgstv@yandex.ru>
2024-02-13 00:16:17 +0300

    add saving params

--- src/model/ModelManager.py
+++ src/model/ModelManager.py
@@ -2,7 +2,22 @@ from abc import ABC, abstractmethod
 
 
 class ModelManager:
-    def __init__(self, model_name) -> None:
+    _model_name_sep = "_"
+    _save_name = "model.keras"
+    _arch_name = "architecture.json"
+    _summary_name = "summary.txt"
+    _plot_name = "plot.png"
+
+    @property
+    def model(self):
+        return self._model
+
+    @model.setter
+    def model(self, value):
+        self._model = value
+
+    def __init__(self, model_name: str, mode: str) -> None:
+        self._model = None
         super().__init__()
 
     def save_model(self):
