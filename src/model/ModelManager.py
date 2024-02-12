97c19e5291356ed6d58819721aa1614cbbbe388f
Ghost1499 <zgstv@yandex.ru>
2024-02-13 00:48:56 +0300

    add new save model implementation

--- src/model/ModelManager.py
+++ src/model/ModelManager.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 
+import keras.saving
+
 from configs import io_config
 
 
@@ -11,32 +13,26 @@ class ModelManager:
     _summary_name = "summary.txt"
     _plot_name = "plot.png"
 
-    @property
-    def model(self):
-        return self._model
-
-    @model.setter
-    def model(self, value):
-        self._model = value
-
     def __init__(self, model_name: str, model_mode: str) -> None:
-        self._model = None
         self._model_name = model_name
         self._model_mode = model_mode
         super().__init__()
 
-    def save_model(self):
-        pass
+    def save_model(self,model:keras.Model,overwrite=False):
+        save_path = self._model_dir()/self._save_name
+        if not overwrite and save_path.exists():
+            raise Exception("Модель по заданному пути уже существует.",save_path)
+        model.save(save_path)
 
     def load_model(self):
-        pass
+        self.model = keras.saving.load_model(self._model_dir() / self._save_name)
 
     def read_model(self):
-        pass
+        if self.
 
     def write_model(self):
         pass
-
+        
     def _model_dir(self):
         return io_config.MODEL_SAVE_DIR / Path(
             self._model_name_sep.join([self._model_name, self._model_mode])
